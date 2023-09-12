051246feeac9e52e187b7ba4d35c8fb6c3c3b9b9
thibault hercod <hercod.thibault@gmail.com>
2023-09-12 14:18:11 +0200

    add home & forms component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FormsComponent } from './forms/forms.component';
+import { DrawComponent } from './draw/draw.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FormsComponent,
+    DrawComponent
   ],
   imports: [
     BrowserModule,
